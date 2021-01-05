ee6f1cd5ee73dcbfea58009a9908778ddcf3eca7
DiegoSPB <diego.spb@gmail.com>
2021-01-04 17:57:54 -0800

    use fauna

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,9 @@
-import { PrismaClient } from "@prisma/client"
-export * from "@prisma/client"
+import { GraphQLClient } from "graphql-request"
 
-let prisma: PrismaClient
+const graphQLClient = new GraphQLClient("https://graphql.fauna.com/graphql", {
+  headers: {
+    authorization: "Bearer " + process.env.FAUNA_SECRET,
+  },
+})
 
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
-} else {
-    // Ensure the prisma instance is re-used during hot-reloading
-    // Otherwise, a new client will be created on every reload
-    globalThis["prisma"] = globalThis["prisma"] || new PrismaClient()
-    prisma = globalThis["prisma"]
-}
-
-export default prisma
+export default graphQLClient
